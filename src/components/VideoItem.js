07cb0676116c0ec0a5d99d9c71149f6d59ff9e0c
Amy <amycheck8715@gmail.com>
2021-12-23 07:07:36 -0500

    cleaned up needless CSS properties

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -7,14 +7,10 @@ import { useData } from '../context/DataContext';
 const StyledPaper = styled(Paper)({
     padding: '1rem',
     margin: '1rem',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'start',
     backgroundColor: '#171515',
     color: 'white',
     overflow: 'hidden',
-    overflowX: 'scroll',
+    width: 'max-content',
 });
 
 const StyledDiv = styled('div')({
